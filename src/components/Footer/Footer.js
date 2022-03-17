fd37f687f549180f9df35880f5d04ed5ba89a8ca
facu09 <fcigliuti@gmail.com>
2022-03-17 02:45:51 -0300

    11vo Commit - Avanzo nro version a 2.043

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -26,7 +26,7 @@ const Footer = () => {
       </div>
        {/* <!-- segunda linea del footer --> */}
       {/* <!-- <div className="Contenedor-Version">ver 1.5</div> --> */}
-      <div className="conteiner-linea1-footer Contenedor-Version">ver 2.041</div>
+      <div className="conteiner-linea1-footer Contenedor-Version">ver 2.043</div>
   </footer>
 
   {/* <!-- segunda linea del footer --> */}
